b9bb2596837c019d07b833d25505fe78a5ebac8a
sumitsprint <ncr2099@gmail.com>
2025-10-26 12:51:35 +0530

    stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+
+   const formatTime = (totalSeconds) => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const secs = totalSeconds % 60;
+    return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+   }
+   useEffect(() => {if(isRunning){
+    timerRef.current = setTimeout(() => {setSeconds(pre => pre + 1)}, 1000);
+   }
+  return ()  => clearTimeout(timerRef.current)
+  }, [ seconds, isRunning]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+       <div>
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(seconds)}</p>
+
+      {isRunning ? (<button onClick = {() => setIsRunning(false)} style = {{marginRight: "8px", backgroundColor: "blue"}}>Stop</button>) : (<button onClick={() => setIsRunning(true)} style = {{marginRight: "8px", backgroundColor: "blue"}}>Start</button>)}
+      <button onClick = {() => { clearTimeout(timerRef.current); setSeconds(0); setIsRunning(false);}} style = {{backgroundColor: "blue"}}>Reset</button>
+    </div>
   )
 }
 
